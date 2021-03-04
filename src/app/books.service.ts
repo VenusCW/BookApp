a9eeb2b048e209f9088ed153f2bbe8a5d1045f4e
Charissa Wersen <charissa.wersen@gmail.com>
2021-03-03 20:49:45 -0500

    charissa added new URL for testing

--- src/app/books.service.ts
+++ src/app/books.service.ts
@@ -1,25 +1,25 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'; 
+import {HttpClient} from '@angular/common/http';
 
 interface Response {
   //results: Book[];
-  title: string; 
+  title: string;
 }
 
 @Injectable({
-  providedIn: 'root', 
+  providedIn: 'root',
 })
 export class BooksService {
     apiKey = "AIzaSyDg4C1YgpmGasG5b6RaoBMmMX-WWWxuzpM";
     //appId = "3096e7e1";
-    url = "https://www.googleapis.com/apiName/apiVersion/resourcePath?parameters";
+    url = "https://www.googleapis.com/books/v1/volumes?q=roots+intitle";
     //public books: Book[];
     //favorites: any[] = [];
     constructor(private http: HttpClient) {}
 
-    getBooks() { return this.http.get(this.urlString).subscribe( (data) => { console.log(data); }, (error) => console.log(error) ); } }
-  
-      
+    getBooks() { return this.http.get(this.url).subscribe( (data) => { console.log(data); }, (error) => console.log(error) ); } }
+
+
       // this.http.get(requestUrl).subscribe(
       //   (response: Response) => {
       //     console.log(response.results);
