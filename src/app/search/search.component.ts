48d269ac8a45bd5af8850678a0ea04ce9cf01aeb
Charissa Wersen <charissa.wersen@gmail.com>
2021-03-03 21:17:55 -0500

    pushing URL & component changes

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { BooksService } from '../books.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
+  providers: [BooksService],
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  constructor(public BooksService: BooksService) {}
 
   ngOnInit(): void {
+    this.BooksService.getBooks();
   }
-
 }
