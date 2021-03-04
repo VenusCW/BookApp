0f1254819488be024aa7ff0483b0c613617796f6
Christina Evans <chirstina.evans@perficient.com>
2021-03-03 20:15:34 -0500

    adding changes

---
+++ src/app/books.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'; 
+
+interface Response {
+  //results: Book[];
+  title: string; 
+}
+
+@Injectable({
+  providedIn: 'root', 
+})
+export class BooksService {
+    apiKey = "AIzaSyDg4C1YgpmGasG5b6RaoBMmMX-WWWxuzpM";
+    //appId = "3096e7e1";
+    url = "https://www.googleapis.com/apiName/apiVersion/resourcePath?parameters";
+    //public books: Book[];
+    //favorites: any[] = [];
+    constructor(private http: HttpClient) {}
+
+    getBooks() { return this.http.get(this.urlString).subscribe( (data) => { console.log(data); }, (error) => console.log(error) ); } }
+  
+      
+      // this.http.get(requestUrl).subscribe(
+      //   (response: Response) => {
+      //     console.log(response.results);
+      //     //this.movies = response.results;
+      //     const newBook = response.results;
+
