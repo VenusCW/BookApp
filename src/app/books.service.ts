f563f54c50aab4ce6163da2511c77b75d1de6cd1
Christina Evans <chirstina.evans@perficient.com>
2021-03-04 21:07:53 -0500

    adding comp changes

--- src/app/books.service.ts
+++ src/app/books.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 interface Response {
-  results: Book[];
+  // results: Book[];
   title: string;
   authors: string;
   thumbnail: string;
@@ -24,7 +24,7 @@ export class BooksService {
     //favorites: any[] = [];
     constructor(private http: HttpClient) {}
 
-    // getBooks() { return this.http.get(this.url).subscribe( (data) => { console.log(data); }, (error) => console.log(error) ); } }
+    getBooks() { return this.http.get(this.url).subscribe( (data) => { console.log(data); }, (error) => console.log(error) ); } }
 
     // getBooks() {
     //   const url = 'https://www.reddit.com/r/aww/.json';
