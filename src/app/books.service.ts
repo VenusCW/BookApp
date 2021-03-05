f14343b711d420f327f09b65b3d639cd1849fe8f
Charissa Wersen <charissa.wersen@gmail.com>
2021-03-04 20:08:13 -0500

    updates so heroku will work

--- src/app/books.service.ts
+++ src/app/books.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 interface Response {
-  results: Book[];
+  // results: Book[];
   title: string;
   authors: string;
   thumbnail: string;
