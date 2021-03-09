8a5759cac1dfdab53007fb30591ceecde3a0ac18
liz <elizabeth.collacott2fcagroup.com>
2021-03-08 21:09:03 -0500

    accepting incoming

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const app = express();
 const profile = require("./routes");
 
@@ -8,7 +9,30 @@ app.use(cors());
 app.use("/", profile);
 app.use(express.json());
 
-app.use(express.static(__dirname + "/public")); 
+app.use(express.static(__dirname + "/public"));
 
 const port = 3000;
 app.listen(port, () => console.log(`Listening on port: ${port}.`));
+
+// const express = require("express");
+// const proxy = require("express-http-proxy");
+
+// const app = express();
+// const port = process.env.PORT || 8080;
+// const cors = require("cors");
+
+// app.use(cors());
+// app.use(express.json());
+
+// const url = "https://www.googleapis.com/books/v1/volumes";
+
+// app.use(
+//   "/",
+//   proxy(url, {
+//     userResHeaderDecorator: () => ({ "Access-Control-Allow-Origin": "*" }),
+//   })
+// );
+
+// app.listen(port, (_) => {
+//   console.log(`Listening on http://localhost:${port}`);
+// });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http'; 
+
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { ExistingLibraryComponent } from './existing-library/existing-library.component';
 import { AddLibraryComponent } from './add-library/add-library.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
     ExistingLibraryComponent,
-    AddLibraryComponent
+    AddLibraryComponent,
+    NotFoundComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule, 
-    AppRoutingModule, 
+    FormsModule,
+    AppRoutingModule,
     HttpClientModule
   ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
