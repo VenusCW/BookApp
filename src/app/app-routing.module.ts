e255fb347ef69a74cc3173e3b4495a41e07abed9
liz <elizabeth.collacott2fcagroup.com>
2021-03-08 21:31:49 -0500

    removed conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,49 +1,23 @@
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-import { Routes, RouterModule } from '@angular/router';
-=======
-<<<<<<< HEAD
 import { CommonModule } from '@angular/common';
-import {Routes, RouterModule} from '@angular/router';
-=======
-import { Routes, RouterModule } from '@angular/router';
->>>>>>> a878b5e61c9918456ed7cce279b97b799657d662
->>>>>>> master
+import { Routes, RouterModule} from '@angular/router';
 
 import { SearchComponent } from './search/search.component';
 import { ExistingLibraryComponent } from './existing-library/existing-library.component';
 import { AddLibraryComponent } from './add-library/add-library.component';
-<<<<<<< HEAD
+
 import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [
-=======
-<<<<<<< HEAD
 import {LandingComponent} from './landing/landing.component';
 
-=======
-import { NotFoundComponent } from './not-found/not-found.component';
->>>>>>> master
-
 const routes: Routes = [
-<<<<<<< HEAD
+
   {path: "", component: SearchComponent},
   {path: "add-library", component: AddLibraryComponent},
   {path: "existing-library", component: ExistingLibraryComponent},
-<<<<<<< HEAD
   {path: "landing", component: LandingComponent},
-=======
-=======
->>>>>>> master
-  { path: '', component: SearchComponent },
-  { path: 'add-library', component: AddLibraryComponent },
-  { path: 'existing-library', component: ExistingLibraryComponent },
   { path: '**', component: NotFoundComponent },
-<<<<<<< HEAD
-=======
->>>>>>> a878b5e61c9918456ed7cce279b97b799657d662
->>>>>>> master
->>>>>>> master
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 
 import { HttpClientModule } from '@angular/common/http';
-=======
-<<<<<<< HEAD
-import {HttpClientModule} from '@angular/common/http';
-=======
-
-import { HttpClientModule } from '@angular/common/http';
->>>>>>> a878b5e61c9918456ed7cce279b97b799657d662
->>>>>>> master
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { ExistingLibraryComponent } from './existing-library/existing-library.component';
 import { AddLibraryComponent } from './add-library/add-library.component';
-<<<<<<< HEAD
+
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NavigationComponent } from './navigation/navigation.component';
-=======
-<<<<<<< HEAD
+
 import {LandingComponent} from './landing/landing.component';
-=======
-<<<<<<< HEAD
->>>>>>> master
-import {FormsModule} from '@angular/forms';
-=======
-import { NotFoundComponent } from './not-found/not-found.component';
-import { NavigationComponent } from './navigation/navigation.component';
->>>>>>> a878b5e61c9918456ed7cce279b97b799657d662
->>>>>>> master
+
 
 @NgModule({
   declarations: [
@@ -40,25 +22,19 @@ import { NavigationComponent } from './navigation/navigation.component';
     SearchComponent,
     ExistingLibraryComponent,
     AddLibraryComponent,
-<<<<<<< HEAD
     NotFoundComponent,
-    NavigationComponent
-=======
-<<<<<<< HEAD
+    NavigationComponent,
     LandingComponent,
-=======
-    NotFoundComponent,
-    NavigationComponent
->>>>>>> master
->>>>>>> master
+    
   ],
+  
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  
   providers: [],
   bootstrap: [AppComponent],
 })
