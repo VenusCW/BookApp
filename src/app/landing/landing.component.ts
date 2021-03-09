e8490d2961a942758e39737fb400941dc45ea866
liz <elizabeth.collacott2fcagroup.com>
2021-03-08 21:17:19 -0500

    for goodness sakes

---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { BooksService } from '../books.service';
+import {Routes} from '@angular/router';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.css'],
+  providers: [BooksService],
+})
+export class LandingComponent implements OnInit {
+  constructor(public BooksService: BooksService) {}
+
+  ngOnInit(): void {
+    this.BooksService.getBooks();
+  }}
+//   const randomQuotes = require('request');
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote',
+//     qs: {token: 'ipworld.info'},
+//     headers: {
+//       'x-rapidapi-key': '3e4bb66922mshd5db928295295c3p1def1bjsncfaf9e1565bc',
+//       'x-rapidapi-host': 'quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com',
+//       useQueryString: true
+//     }
+//   };
+
+//   request(options, function (error, response, body) {
+//       if (error) throw new Error(error);
+
+//       console.log(body);
+//   });
+
