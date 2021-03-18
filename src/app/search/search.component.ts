a21f111631fe7dd554ba0737e74c026899fdddf0
Charissa Wersen <charissa.wersen@gmail.com>
2021-03-18 19:30:01 -0400

    pushing changes

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { BooksService } from '../books.service';
 import { FormControl } from '@angular/forms';
+import { LibraryService } from '../library.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
-  providers: [BooksService],
+  // providers: [BooksService],
 })
 export class SearchComponent implements OnInit {
   books: any;
@@ -14,25 +15,43 @@ export class SearchComponent implements OnInit {
   public generalSearch: string = '';
   public author: string = '';
   public subjectSearch: string = '';
+  public title: string = '';
+
 
   getBooksList(): void {
     this.BooksService.getBooks(
-      this.generalSearch,
+      this.generalSearch
       // this.author,
       // this.subjectSearch
     );
   }
 
-
-  constructor(public BooksService: BooksService) {}
+  constructor(
+    public BooksService: BooksService,
+    public library: LibraryService
+  ) {}
 
   ngOnInit() {
     // this.BooksService.getBooks();
   }
   goToLink(url: string) {
-    window.open(url, "_blank");
+    window.open(url, '_blank');
   }
-  addToLibrary(): void {
 
+// setBookDetails() {
+//     this.library.title = this.BooksService.volumeInfo.title;
+    // this.author = this.author;
+    // this.category = this.category;
+  // }
+  addFromSearch() {
+    const newSearchBook = {
+      readername: this.library.readername,
+      status: 'Wishlist',
+      title: this.title,
+      // author: this.BooksService.author,
+      // category: this.BooksService.category,
+    };
+    console.log(this.title); 
+    this.library.addFromSearch(newSearchBook);
   }
 }
